8b53a4dfb0aeb3e1d8938636cba65a6fdced82f0
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-04 22:00:58 +0300

    function declatiom,functuion calling,and function return.

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
+const { DiffieHellmanGroup } = require("crypto");
+const { console } = require("inspector");
+
 // Follow along with the examples here
+//function definination /function declaration
+function doNothing(){};
+function sayHello(){
+  console.log("Hello!");
+}
+sayHello();
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+    //function call
+    sayHelloToGuadalupe();
+
+    function sayHelloToGuadalupe() {
+  
+        console.log("Hello, Guadalupe!");
+    } 
+     //function call
+        sayHelloToGuadalupe();
+
+    function sayHelloToSamip() {
+        console.log("Hello, Samip!");
+      }
+      //function call
+      sayHelloToSamip();
+
+      function doSomething(thing) {
+        console.log(thing);
+      }
+      //function call
+      doSomething("anything");
+
+      function say( greeting,firstName){
+        return `${greeting},${firstName}!`;
+      }
+      console.log(say("Hello", "Liz"));
+      //console.log(say("Hello","liz"));
+        //sayHelloTo("Guadalupe");
+        //sayHelloTo("jane");
+        //sayHelloTo( "R2-R2");
+        //sayHelloTo9("1");
+        //node.index.js
+       // function say(goodBye, julio) {
+          //return `${greeting}, ${firstName}!`;
+         // console.log(`${greeting}, ${firstName}!`);
+        
+        //console.log("say goodBye julio");
+
+        
+      //return fuction
+      //console.log(firstName);
+      function add( x,y){
+        return 1 + 2 ;
+        console.log(add(1,2)) ;
+
+      }
+      console.log(add(1,2));
+
+// return function
+function add( x,y){
+    return 80 + 9000;
+    console .log (add(80,9000));
+
+}
+console .log (add(80,9000));
